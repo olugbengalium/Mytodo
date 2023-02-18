149f499b58de0e7357d06e9f622f7a0ee51e88b9
Akinbande Gbenga <olugbengalium@gmail.com>
2023-02-18 19:33:30 +0100

    Initial Commit

---
+++ Mytodo/MytodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MytodoApp.swift
+//  Mytodo
+//
+//  Created by Gbenga Abayomi on 18/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct MytodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
